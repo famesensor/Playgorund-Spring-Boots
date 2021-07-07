e6d19c65afc5645972eac28f22ae3387bfd7c589
famesensor <sathit.ju@outlook.com>
2021-07-08 00:13:37 +0700

    [:tada:]: init commiy first

---
+++ src/main/java/com/example/PlaygroundSpringBoot/PlaygroundSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.PlaygroundSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaygroundSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PlaygroundSpringBoot/PlaygroundSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PlaygroundSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
