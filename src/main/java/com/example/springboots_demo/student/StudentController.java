0f6aa04a0ae96f05911d02881cd99e9361a06dfa
famesensor <sathit.ju@outlook.com>
2021-07-12 23:28:54 +0700

    [:hammer:]: init student controller

--- src/main/java/com/example/springboots_demo/student/StudentController.java
+++ src/main/java/com/example/springboots_demo/student/StudentController.java
@@ -1,2 +1,18 @@
-package com.example.springboots_demo.student;public class StudentController {
+package com.example.springboots_demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = " ")
+public class StudentController {
+    @GetMapping
+    public List<Student> getStudents() {
+        return List.of(new Student(1L, "Fame", "fame@mail.com", LocalDate.of(1998, Month.DECEMBER, 25), 22));
+    }
 }
