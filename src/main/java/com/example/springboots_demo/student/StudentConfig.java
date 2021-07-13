7cc8e030b72d34224382b97f0d03a732c7a0b31a
famesensor <sathit.ju@outlook.com>
2021-07-13 22:58:49 +0700

    [:hammer:]: update config student

--- src/main/java/com/example/springboots_demo/student/StudentConfig.java
+++ src/main/java/com/example/springboots_demo/student/StudentConfig.java
@@ -1,2 +1,23 @@
-package com.example.springboots_demo.student;public class StudentConfig {
+package com.example.springboots_demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return  args -> {
+           Student fame = new Student("Fame","fame@mail.com", LocalDate.of(1998, Month.DECEMBER,25), 22);
+           Student sensor = new Student("Sensor", "sensor@gmail.com",LocalDate.of(1998, Month.DECEMBER,25), 22);
+
+           repository.saveAll(List.of(fame,sensor));
+        };
+    }
 }
