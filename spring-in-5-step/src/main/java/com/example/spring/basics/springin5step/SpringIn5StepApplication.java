829602434ec9500fa97bb645268982ee652acad8
famesensor <sathit.ju@outlook.com>
2021-07-26 21:05:51 +0700

    [:tada:]: init commit spring basic

---
+++ spring-in-5-step/src/main/java/com/example/spring/basics/springin5step/SpringIn5StepApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.basics.springin5step;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIn5StepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIn5StepApplication.class, args);
+	}
+
+}
