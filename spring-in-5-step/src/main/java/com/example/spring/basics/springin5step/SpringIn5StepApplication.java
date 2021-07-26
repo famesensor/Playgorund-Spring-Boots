2e26f1a0aa71453a0cdb7f47e32b5d1f4d33b803
famesensor <sathit.ju@outlook.com>
2021-07-26 22:49:19 +0700

    [:hammer:]: add bean in context

--- spring-in-5-step/src/main/java/com/example/spring/basics/springin5step/SpringIn5StepApplication.java
+++ spring-in-5-step/src/main/java/com/example/spring/basics/springin5step/SpringIn5StepApplication.java
@@ -2,12 +2,21 @@ package com.example.spring.basics.springin5step;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringIn5StepApplication {
 
+
+
 	public static void main(String[] args) {
-		SpringApplication.run(SpringIn5StepApplication.class, args);
+		// BinarySearchImpl binarySearch = new BinarySearchImpl(new BubbleSortAlgorithm()); // new QuickSortAlgorithm
+
+		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepApplication.class, args);
+		BinarySearchImpl binarySearch =	applicationContext.getBean(BinarySearchImpl.class);
+
+		int res = binarySearch.binarySearch(new int[]{12, 4}, 4);
+		System.out.println(res );
 	}
 
 }
