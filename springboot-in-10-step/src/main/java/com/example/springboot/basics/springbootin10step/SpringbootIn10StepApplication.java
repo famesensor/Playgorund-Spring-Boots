371f94eb34f09c32f8e466251ebb0b46ac751c20
famesensor <sathit.ju@outlook.com>
2021-07-28 18:47:43 +0700

    [:hammer:]: add print bean

--- springboot-in-10-step/src/main/java/com/example/springboot/basics/springbootin10step/SpringbootIn10StepApplication.java
+++ springboot-in-10-step/src/main/java/com/example/springboot/basics/springbootin10step/SpringbootIn10StepApplication.java
@@ -2,12 +2,17 @@ package com.example.springboot.basics.springbootin10step;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringbootIn10StepApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootIn10StepApplication.class, args);
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringbootIn10StepApplication.class, args);
+
+		for (String name : applicationContext.getBeanDefinitionNames()) {
+			System.out.println(name);
+		}
 	}
 
 }
