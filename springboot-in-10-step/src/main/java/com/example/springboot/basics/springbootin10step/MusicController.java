eac13c8b445500aec3518eb796e833499a17cd9d
famesensor <sathit.ju@outlook.com>
2021-07-28 18:47:16 +0700

    [:hammer:]: init controller

--- springboot-in-10-step/src/main/java/com/example/springboot/basics/springbootin10step/MusicController.java
+++ springboot-in-10-step/src/main/java/com/example/springboot/basics/springbootin10step/MusicController.java
@@ -1,2 +1,17 @@
-package com.example.springboot.basics.springbootin10step;public class MusicController {
+package com.example.springboot.basics.springbootin10step;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class MusicController {
+
+    @GetMapping("/musics")
+    public List<Music> getMusics() {
+        return Arrays.asList(new Music(1234l, "Enter sandman", "metaliga"));
+    }
+
 }
