0cb67ea198879b2641c395fb164a58ee3d11e684
famesensor <sathit.ju@outlook.com>
2021-07-28 18:47:00 +0700

    [:hammer:]: init model music

--- springboot-in-10-step/src/main/java/com/example/springboot/basics/springbootin10step/Music.java
+++ springboot-in-10-step/src/main/java/com/example/springboot/basics/springbootin10step/Music.java
@@ -1,2 +1,18 @@
-package com.example.springboot.basics.springbootin10step;public class Music {
+package com.example.springboot.basics.springbootin10step;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Music {
+    long id;
+    String name;
+    String band;
+
+    public Music(long id, String name, String band) {
+        this.id = id;
+        this.name = name;
+        this.band = band;
+    }
 }
