5301087bbb6982cf6d8a3bdfd031d9035a405139
famesensor <sathit.ju@outlook.com>
2021-07-31 17:35:59 +0700

    [:hammer:]: update model exception

--- restapi-service/src/main/java/com/example/rest/webservice/restapiservice/exception/ExceptionResponse.java
+++ restapi-service/src/main/java/com/example/rest/webservice/restapiservice/exception/ExceptionResponse.java
@@ -1,4 +1,20 @@
-package com.example.rest.webservice.restapiservice.controller;
+package com.example.rest.webservice.restapiservice.exception;
 
-public class ExceptionResponse {
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class ExceptionResponse  {
+    private Date timestamp;
+    private String message;
+    private String detail;
+
+    public ExceptionResponse(Date timestamp, String message, String detail) {
+        this.timestamp = timestamp;
+        this.message = message;
+        this.detail = detail;
+    }
 }
