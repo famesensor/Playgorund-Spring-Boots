747925b4a394a02f005e52815ec5365db44da044
famesensor <sathit.ju@outlook.com>
2021-07-31 16:42:08 +0700

    [:tada:]: init commit handle exception

---
+++ restapi-service/src/main/java/com/example/rest/webservice/restapiservice/exception/ExceptionResponse.java
@@ -0,0 +1,4 @@
+package com.example.rest.webservice.restapiservice.controller;
+
+public class ExceptionResponse {
+}
---
+++ restapi-service/src/main/java/com/example/rest/webservice/restapiservice/exception/ResponseExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.example.rest.webservice.restapiservice.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+@RestController
+public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleException(Exception ex, WebRequest request) {
+        new ExceptionResponse(new Date(), ex.getMessage(),"" );
+    }
+}
