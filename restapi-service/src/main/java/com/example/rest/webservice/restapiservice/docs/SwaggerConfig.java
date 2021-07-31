801b21142e927490059b2672e645310a41de6a9b
famesensor <sathit.ju@outlook.com>
2021-07-31 17:35:23 +0700

    [:truck:]: move file

--- restapi-service/src/main/java/com/example/rest/webservice/restapiservice/docs/SwaggerConfig.java
+++ restapi-service/src/main/java/com/example/rest/webservice/restapiservice/docs/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.example.rest.webservice.restapiservice;
+package com.example.rest.webservice.restapiservice.docs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
