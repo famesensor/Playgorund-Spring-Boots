6d99ec17417a6538c0e6722ad2de07faa142df44
famesensor <sathit.ju@outlook.com>
2021-07-31 14:37:06 +0700

    [:tada:]: init commit sprint boot rest api

---
+++ restapi-service/src/main/java/com/example/rest/webservice/restapiservice/RestapiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.rest.webservice.restapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiServiceApplication.class, args);
+	}
+
+}
