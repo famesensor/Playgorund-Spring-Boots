393da185f23290b8fba452bb3f8b30832e5adc62
famesensor <sathit.ju@outlook.com>
2021-07-31 17:36:43 +0700

    [:hammer:]: update user service

--- restapi-service/src/main/java/com/example/rest/webservice/restapiservice/service/UserService.java
+++ restapi-service/src/main/java/com/example/rest/webservice/restapiservice/service/UserService.java
@@ -1,13 +1,12 @@
-package com.example.rest.webservice.restapiservice.user.service;
+package com.example.rest.webservice.restapiservice.service;
 
-import com.example.rest.webservice.restapiservice.user.dto.User;
+import com.example.rest.webservice.restapiservice.controller.UserNotFoundException;
+import com.example.rest.webservice.restapiservice.dto.User;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 @Component
 public class UserService {
@@ -15,17 +14,25 @@ public class UserService {
 
     private static int userCount = 2;
     static {
-        users.add(new User(1, "Fame", new Date(25,12,1998)));
-        users.add(new User(2,"Sensor", new Date(27,12,1999)));
+        users.add(new User(1, "Fame", new Date()));
+        users.add(new User(2,"Sensor", new Date()));
     }
 
     public List<User> findAll() {
         return users;
     }
 
-    public void save(User user) {
-        if (user.getId() == null) user.setId(userCount++);
+    public User save(User user) {
+        if (user.getId() == null) user.setId(++userCount);
         users.add(user);
+        return user;
+    }
+
+    public int deleteUserById(int id) {
+          User user = users.stream().filter(ele -> ele.getId() == id).findAny().orElse(null);
+          if (user == null) throw new UserNotFoundException("not found");
+          users.remove(user);
+          return user.getId();
     }
 
     public User findById(int id) {
