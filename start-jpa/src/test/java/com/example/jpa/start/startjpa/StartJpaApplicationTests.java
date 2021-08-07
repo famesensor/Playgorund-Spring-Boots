5ee34160def74b8ec94c8cad178ba4337b2cb6e6
famesensor <sathit.ju@outlook.com>
2021-08-07 21:20:06 +0700

    [:tada:]: init commit start jpa

---
+++ start-jpa/src/test/java/com/example/jpa/start/startjpa/StartJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa.start.startjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
