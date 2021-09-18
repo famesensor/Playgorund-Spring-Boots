86955847f7835baa326ef6c3762775a422484768
famesensor <sathit.ju@outlook.com>
2021-09-18 17:46:50 +0700

    [:hammer:]: implement service blog and blog category

--- spring-boot-kotlin/src/main/kotlin/com/example/spring/springBootKotlinApp/services/BlogCategoryServiceImpl.kt
+++ spring-boot-kotlin/src/main/kotlin/com/example/spring/springBootKotlinApp/services/BlogCategoryServiceImpl.kt
@@ -1,4 +1,51 @@
 package com.example.spring.springBootKotlinApp.services
 
-class BlogCategoryServiceImpl {
+import com.example.spring.springBootKotlinApp.entities.BlogCategory
+import com.example.spring.springBootKotlinApp.model.blogcategory.BlogCategoryRequest
+import com.example.spring.springBootKotlinApp.repositories.BlogCategoryRepository
+import org.hibernate.annotations.NotFound
+import org.hibernate.annotations.common.util.impl.LoggerFactory.logger
+import org.slf4j.Logger
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
+
+@Service
+class BlogCategoryServiceImpl @Autowired constructor(val blogCategoryRepository: BlogCategoryRepository) :
+    BlogCategoryService {
+
+    override fun createBlogCategory(body: BlogCategoryRequest): BlogCategory {
+        var blogCategory = BlogCategory(name = body.name);
+        return blogCategoryRepository.save(blogCategory)
+    }
+
+    override fun updateBlogCategory(id: Long, body: BlogCategoryRequest): BlogCategory {
+//        var blogCategory = blogCategoryRepository.findById(id).map { item -> {
+//            val updateBlogCategory = item.copy(name = body.name)
+//
+//            blogCategoryRepository.save(updateBlogCategory)
+//        } }.orElse { throw ResponseStatusException(HttpStatus.NOT_FOUND, "Not found blog category") }
+
+        var blogCategory = blogCategoryRepository.findById(id)
+
+        if (!blogCategory.isPresent) throw ResponseStatusException(HttpStatus.NOT_FOUND, "Not found blog category")
+
+        var updateBlog = blogCategory.get()
+        updateBlog.name = body.name
+        return blogCategoryRepository.save(updateBlog)
+    }
+
+    override fun getBlogCategories(): List<BlogCategory> {
+        return blogCategoryRepository.findAll()
+    }
+
+    override fun getBlogCategory(id: Long): BlogCategory {
+        var blogCategory = blogCategoryRepository.findById(id)
+
+        if (!blogCategory.isPresent) throw ResponseStatusException(HttpStatus.NOT_FOUND, "Not found blog category")
+
+        return blogCategory.get()
+    }
 }
--- spring-boot-kotlin/src/main/kotlin/com/example/spring/springBootKotlinApp/services/BlogServiceImpl.kt
+++ spring-boot-kotlin/src/main/kotlin/com/example/spring/springBootKotlinApp/services/BlogServiceImpl.kt
@@ -1,4 +1,52 @@
 package com.example.spring.springBootKotlinApp.services
 
-class BlogServiceImpl {
+import com.example.spring.springBootKotlinApp.entities.Blog
+import com.example.spring.springBootKotlinApp.entities.BlogCategory
+import com.example.spring.springBootKotlinApp.model.blog.BlogRequest
+import com.example.spring.springBootKotlinApp.repositories.BlogCategoryRepository
+import com.example.spring.springBootKotlinApp.repositories.BlogRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
+import java.util.*
+
+@Service
+class BlogServiceImpl @Autowired constructor(val blogRepository: BlogRepository, val blogCategoryService: BlogCategoryService) : BlogService {
+    override fun createBlog(body: BlogRequest): Blog {
+        var blogCategory = blogCategoryService.getBlogCategory(body.blogCategoryId)
+
+        var blog: Blog = Blog(title = body.title, detail = body.detail, blogCategory = blogCategory)
+
+        return blogRepository.save(blog)
+    }
+
+    override fun updateBlog(id: Long, body: BlogRequest): Blog {
+        var updateBlog: Blog = getBlog(id)
+
+        updateBlog.title = body.title
+        updateBlog.detail = body.detail
+
+        if (updateBlog.blogCategory?.id != body.blogCategoryId) updateBlog.blogCategory = blogCategoryService.getBlogCategory(body.blogCategoryId)
+
+        return blogRepository.save(updateBlog)
+    }
+
+    override fun getBlog(id: Long): Blog {
+        var blog: Optional<Blog> = blogRepository.findById(id)
+
+        if (!blog.isPresent) throw ResponseStatusException(HttpStatus.NOT_FOUND, "Not found blog")
+
+        return blog.get()
+    }
+
+    override fun deleteBlog(id: Long) {
+        var blog: Blog = getBlog(id)
+
+        return blogRepository.delete(blog)
+    }
+
+    override fun getBlogs(): List<Blog> {
+        return blogRepository.findAll()
+    }
 }
