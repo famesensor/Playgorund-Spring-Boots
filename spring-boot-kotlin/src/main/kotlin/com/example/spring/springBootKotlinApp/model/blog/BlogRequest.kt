226973ed06586ea654cd3676a75da98db3ca3e70
famesensor <sathit.ju@outlook.com>
2021-09-18 17:46:07 +0700

    [:hammer:]: fix type body request

--- spring-boot-kotlin/src/main/kotlin/com/example/spring/springBootKotlinApp/model/blog/BlogRequest.kt
+++ spring-boot-kotlin/src/main/kotlin/com/example/spring/springBootKotlinApp/model/blog/BlogRequest.kt
@@ -1,3 +1,3 @@
 package com.example.spring.springBootKotlinApp.model.blog
 
-data class BlogRequest(var title: String, var detail: String, var blogCategoryId: String)
+data class BlogRequest(var title: String, var detail: String, var blogCategoryId: Long)
